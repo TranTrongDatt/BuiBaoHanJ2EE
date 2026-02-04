e62151011e42f9eb4a4c9f176f123abed5844caa
TranTrongDatt <bbaohan2212@gmail.com>
2026-02-04 17:02:56 +0700

    Initial commit: Spring Boot Book Management System

---
+++ src/main/java/fit/hutech/BuiBaoHan/BuiBaoHanApplication.java
@@ -0,0 +1,13 @@
+package fit.hutech.BuiBaoHan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuiBaoHanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuiBaoHanApplication.class, args);
+	}
+
+}
